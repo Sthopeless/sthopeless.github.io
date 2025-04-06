bab23484e4a6cf6771abb1da093fd55a77c794be
Sthopeless <6695214+Sthopeless@users.noreply.github.com>
2025-04-06 11:15:04 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    document.body.classList.toggle('retro');
+}
+
+// Game initialization placeholder
+window.onload = () => {
+    document.getElementById("game-container").innerHTML = "<p>Welcome to Drug Wars. Choose your city to start dealing.</p>";
+};
